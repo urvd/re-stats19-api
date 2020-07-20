acae25c8397bd62f03edef5c5cb7420bd1ad58c3
uriel <uriel.vido@hotmail.fr>
2020-07-20 23:46:07 +0200

    first commit

---
+++ src/main/java/com/backend/restats19api/ReStats19ApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.restats19api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReStats19ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReStats19ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/restats19api/ReStats19ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.restats19api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReStats19ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
