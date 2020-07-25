a6185c22dd69ca98baecfa141b9f6a07499f47d1
uriel <uriel.vido@hotmail.fr>
2020-07-25 14:21:39 +0200

    routing fixed

--- src/main/java/com/backend/restats19api/ReStats19ApiApplication.java
+++ src/main/java/com/backend/restats19api/ReStats19ApiApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/")
 public class ReStats19ApiApplication {
-	@GetMapping
+
+	@RequestMapping(value = "/",method = RequestMethod.GET)
 	public String Hello(){
 		return "-- Hello the world !! --";
 	}
