59e015bc32efaaa3f4a440418814e906da946d93
uriel <uriel.vido@hotmail.fr>
2020-07-28 06:42:13 +0200

    rmake get default controller

--- src/main/java/com/backend/restats19api/controllers/DefaultController.java
+++ src/main/java/com/backend/restats19api/controllers/DefaultController.java
@@ -1,5 +1,6 @@
 package com.backend.restats19api.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,8 +9,12 @@ public class DefaultController {
 
     @GetMapping
     @ResponseBody
-    public String Hello(){
-        return "-- Hello the world !! --";
+    public ResponseEntity<?> Hello(){
+        try{
+            return ResponseEntity.ok("-- Hello the world !! --");
+        }catch (Exception e){
+            return  ResponseEntity.ok(e.getCause().getStackTrace());
+        }
     }
 }
 
